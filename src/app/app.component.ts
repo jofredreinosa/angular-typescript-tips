aed9b8ad919808185df855ddb17554f99ce892f4
Jofred <reinosaj@gmail.com>
2024-08-08 17:19:31 -0400

    tips

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private http = inject(HttpClient);
   title = 'typescriptTips';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://randomuser.me/api/?results=15').subscribe((response: any) => {
+      console.log(response.results);
+      this.users = response.results;
+    })
+  }
 }
